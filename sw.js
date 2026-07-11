42406e2f9c749a16938ee2882ab153f15b3311d3
BurnNih <57341377+BurnNih@users.noreply.github.com>
2026-07-11 20:49:53 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='tiktok-admin-v2';const ASSETS=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
