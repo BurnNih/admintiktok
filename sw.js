9219db6ca56328840d61464755e48172691b7468
BurnNih <57341377+BurnNih@users.noreply.github.com>
2026-09-16 11:44:42 +0700

    .

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tiktok-admin-v12-custom';
+const CACHE = 'tiktok-admin-v14-custom';
 const ASSETS = ['./','./index.html','./manifest.json','./logo1.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
